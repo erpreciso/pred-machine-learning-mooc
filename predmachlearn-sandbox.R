97d8153a9be05cb8b9faee9f9907d897de42ed17
erpreciso <trepreciso@gmail.com>
2015-05-18 16:39:25 +0200

    implement gbm model, and predicted test cases

--- predmachlearn-sandbox.R
+++ predmachlearn-sandbox.R
@@ -1,8 +1,5 @@
 # load libraries
-require(reshape)
-require(ggplot2)
 require(caret)
-require(MASS)
 set.seed(816)
 
 # download data sets
@@ -14,6 +11,7 @@ setwd("/home/erpreciso/Documents/school")
 training <- read.csv("temp", na.strings=c("NA","#DIV/0!"))
 testing <- read.csv("temp2", na.strings=c("NA","#DIV/0!"))
 
+## Preprocessing
 # find predictors with zero / near zero variance
 nsv <- nearZeroVar(training, saveMetrics=TRUE)
 to.eliminate <- row.names(nsv[nsv$nzv,])
@@ -40,39 +38,18 @@ high.cor <- which(training.cor > .8, arr.ind=TRUE)
 training <- training[, -high.cor]
 testing <- testing[, -high.cor]
 
-# # convert testing variable types like training
-# for (i in (1:length(names(testing)))){
-#     testing[,i] <- as.numeric(testing[,i])
-# }
-
-# create a validation subset
-in.train <- createDataPartition(training$classe, p=0.1, list=FALSE)
+# create a validation subset to test different models
+in.train <- createDataPartition(training$classe, p=0.8, list=FALSE)
 ta <- training[in.train,]
 te <- training[-in.train,]
 
+# using cross-validation in the model
+fc <- trainControl(method = "repeatedcv", number = 5, repeats = 5)
+
 # fit simple model
-fc <- trainControl(method = "repeatedcv", number = 10, repeats = 10)
 
-test.fit <- train(classe~.,data=ta, method="rpart", preProcess="pca", trControl=fc)
-confusionMatrix(predict(test.fit, newdata=te), te$classe)
+lda.fit <- train(classe~.,data=ta, method="lda", trControl=fc)
+confusionMatrix(predict(lda.fit, newdata=te), te$classe)
 
-test.fit2 <- train(classe~.,data=ta, method="lda", trControl=fc)
-confusionMatrix(predict(test.fit2, newdata=te), te$classe)
-
-test.fit3 <- train(classe~.,data=ta, method="gbm", trControl=fc)
-confusionMatrix(predict(test.fit3, newdata=te), te$classe)
-
-# create model --> accuracy = 0.40
-fit <- train(classe ~ ., data = training,
-             method = "rpart",
-             preProcess = "pca",
-             trControl = fc)
-predictions <- predict(fit, newdata=testing)
-
-# create model --> accuracy = 0.40
-fit2 <- train(classe ~ ., data = training,
-             method = "lda",
-             preProcess = "pca",
-             trControl = fc)
-predictions2 <- predict(fit2, newdata=testing)
-
+gbm.fit <- train(classe~.,data=ta, method="gbm", trControl=fc)
+confusionMatrix(predict(gbm.fit, newdata=te), te$classe)
